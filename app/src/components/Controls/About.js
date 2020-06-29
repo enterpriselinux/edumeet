948881942a8dd5cb55d3202b47bff3c188774eba
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2020-06-29 09:29:53 +0530

    Update About.js

--- app/src/components/Controls/About.js
+++ app/src/components/Controls/About.js
@@ -75,21 +75,14 @@ const About = ({
 			</DialogTitle>
 			<DialogContent dividers>
 				<DialogContentText paragraph>
-					Contributions to this work were made on behalf of the GÉANT
-					project, a project that has received funding from the
-					European Union’s Horizon 2020 research and innovation
-					programme under Grant Agreement No. 731122 (GN4-2).
-					On behalf of GÉANT project, GÉANT Association is the sole
-					owner of the copyright in all material which was developed
-					by a member of the GÉANT project.
+					Cyber Meeting is a WebRTC based video conferencing system.  
+					It is your smart choice for business meetings and distance learning. 
 				</DialogContentText>
 				<DialogContentText paragraph>
-					GÉANT Vereniging (Association) is registered with the
-					Chamber of Commerce in Amsterdam with registration number
-					40535155 and operates in the UK as a branch of GÉANT
-					Vereniging. Registered office: Hoekenrode 3, 1102BR
-					Amsterdam, The Netherlands. UK branch address: City House,
-					126-130 Hills Road, Cambridge CB2 1PQ, UK.
+					You do not need any client application to use this conferencing system.
+					Just need a modern web browser like Chrome, Firefox, Edge, Opera Etc.
+					Whether you are using a Desktop, Tab, Mobile, or Smart TV will not be a
+					matter of concern to use Cyber Meeting.  
 				</DialogContentText>
 				<DialogContentText align='center' paragraph>
 					<Link href={eduMeetUrl} target='_blank' rel='noreferrer' color='secondary' variant='h6'>
